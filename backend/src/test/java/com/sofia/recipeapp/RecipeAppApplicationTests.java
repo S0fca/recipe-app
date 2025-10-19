7168739d95d33a88725d77a17f2196f8bae0003f
Sofia <sofia.hennelova@gmail.com>
2025-10-19 17:17:40 +0200

    test fixed

--- backend/src/test/java/com/sofia/recipeapp/RecipeAppApplicationTests.java
+++ backend/src/test/java/com/sofia/recipeapp/RecipeAppApplicationTests.java
@@ -1,9 +1,15 @@
 package com.sofia.recipeapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
 class RecipeAppApplicationTests {
 
     @Test
