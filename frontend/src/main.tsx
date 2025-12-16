b1f7cd5ab3163025e01a785dfc88fd69ee7b4a1d
Sofia <sofia.hennelova@gmail.com>
2025-12-16 16:33:15 +0100

    React snowfall added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
+import {Snowfall} from "react-snowfall";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <Snowfall
+          snowflakeCount={250}
+          color={'#ffffff'}
+          speed={[1, 3]}
+          radius={[0.5, 3.5]}
+          style={{
+              position: "fixed",
+              zIndex: 9999,
+              pointerEvents: "none",
+          }}
+      />
     <App />
   </StrictMode>,
 )
