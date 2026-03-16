eb45c492f3273ae501318f1a0cf572baf9fbdf18
Sofia <sofia.hennelova@gmail.com>
2026-03-16 17:00:09 +0100

    Remove snowfall

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,21 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
-import {Snowfall} from "react-snowfall";
+// import {Snowfall} from "react-snowfall";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Snowfall
-          snowflakeCount={150}
-          color={'#ffffff'}
-          speed={[0.5, 2.5]}
-          radius={[0.5, 3.5]}
-          style={{
-              position: "fixed",
-              zIndex: 9999,
-              pointerEvents: "none",
-          }}
-      />
+      {/*<Snowfall*/}
+      {/*    snowflakeCount={150}*/}
+      {/*    color={'#ffffff'}*/}
+      {/*    speed={[0.5, 2.5]}*/}
+      {/*    radius={[0.5, 3.5]}*/}
+      {/*    style={{*/}
+      {/*        position: "fixed",*/}
+      {/*        zIndex: 9999,*/}
+      {/*        pointerEvents: "none",*/}
+      {/*    }}*/}
+      {/*/>*/}
     <App />
   </StrictMode>,
 )
